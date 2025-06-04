d1d76d44fa3d1b2180f8adfd3f2f5e5fb127af6d
davudkII <tataryans.d@gmail.com>
2025-06-05 02:49:53 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,20 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    clean: true
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 };
