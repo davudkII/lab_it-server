dc4a7d334a35b8bb1026acfbb4dfe0b2a1b57de9
davudkII <tataryans.d@gmail.com>
2025-06-05 03:35:10 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,82 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, 'src', 'pages', 'index.js'), // Точка входа
+  
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true
+    filename: 'scripts/[name].[contenthash].js',
+    publicPath: '/',
+    assetModuleFilename: 'images/[name][ext]'
   },
+
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: path.resolve(__dirname, 'src', 'images'), 
+          to: 'images',
+          noErrorOnMissing: true
+        },
+        {
+          from: path.resolve(__dirname, 'src', 'vendor'),
+          to: 'vendor'
+        }
+      ]
     })
   ],
+
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
       }
     ]
+  },
+
+  resolve: {
+    alias: {
+      '@images': path.resolve(__dirname, 'src', 'images'),
+      '@scripts': path.resolve(__dirname, 'src', 'scripts'),
+      '@pages': path.resolve(__dirname, 'src', 'pages')
+    }
   }
 };
